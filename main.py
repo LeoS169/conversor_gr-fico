12c464ea3398639807d1deb4cd6c3fee82d5fe6d
LeoSilva <leonamfrancisco7@gmail.com>
2024-09-24 23:18:08 -0300

    modificação do if

--- main.py
+++ main.py
@@ -10,24 +10,25 @@ O app terá que ter:
 import flet as ft
 
 def main(page: ft.Page): # Criação de página inicial do app
-
     def pick_files_result(e: ft.FilePickerResultEvent): # função para a seleção de arquivos. e contém as informações de selceçãp
+            
+        if e.files: #testa se algum arquivo foi selecionado
 
-        arquivos.value = ( # variável com os arquivos selcecionados
-            ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!" # se arquivos forem selecionados (if e.files)
-            # executa-se a parte de trás. Que percorre cada arquivo (f) da lista (e.files) pegando seu nome e unindo em uma só string.
-            # map(lambda f: f.name, e.files) pega gada nome de arquivo e põe na lista
-            # ','.join() separa os valores por uma virgula
-        )
+            arquivos.value = ','.join(map(lambda f: f.name, e.files))
+                # função lambda irá receber um arquivo f e retornar sei nome (f.name), isso dentro de um map que irá
+                # repetir a função para cada e.files, que aloca os arquivos
+                # o join ira unir cada elemento da lista em um string, separado por uma ,
+            
+        else: 
+            arquivos.value = "Nenhum arquivo selecionado." 
 
         arquivos.update() # atualiza valores da variável de arquivos selecionados
 
     seletor_arquivos = ft.FilePicker(on_result=pick_files_result) # cria o seletor para selecão de arquivos
     # chama a função de verificação de arquivos, ao fim
 
-    arquivos = ft.Text()
-
     page.overlay.append(seletor_arquivos) # adiciona o seletor sobre a página, quando necessário
+    arquivos = ft.Text()
 
     page.add(
 
